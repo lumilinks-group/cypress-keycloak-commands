6353ec87ceeeba9016bed1a7d0e3ba6a06a9fe14
joshmartinlumi <josh.martin@lumilinks.com>
2023-02-16 12:14:19 +0000

    develop: fixed logout

--- dist/kc-logout.js
+++ dist/kc-logout.js
@@ -5,5 +5,25 @@ Cypress.Commands.add("kcLogout", function () {
     var realm = Cypress.env("auth_realm");
     return cy.request({
         url: authBaseUrl + "/realms/" + realm + "/protocol/openid-connect/logout"
+    })
+        .then(function (response) {
+        var html = document.createElement("html");
+        html.innerHTML = response.body;
+        var form = html.getElementsByTagName("form")[0];
+        var inputs = form.getElementsByTagName("input");
+        var actionUrl = form.getAttribute('action');
+        var url = "" + authBaseUrl + actionUrl;
+        var body = {};
+        for (var _i = 0, _a = Array.prototype.slice.call(inputs); _i < _a.length; _i++) {
+            var input = _a[_i];
+            body[input.name] = input.value;
+        }
+        return cy.request({
+            method: "POST",
+            url: url,
+            followRedirect: false,
+            form: true,
+            body: body
+        });
     });
 });
--- src/kc-logout.ts
+++ src/kc-logout.ts
@@ -5,5 +5,25 @@ Cypress.Commands.add("kcLogout", () => {
 
   return cy.request({
     url: `${authBaseUrl}/realms/${realm}/protocol/openid-connect/logout`
+  })
+  .then(function (response) {
+      const html = document.createElement("html");
+      html.innerHTML = response.body;
+      const form = html.getElementsByTagName("form")[0];
+      const inputs = form.getElementsByTagName("input")
+      const actionUrl = form.getAttribute('action');
+      const url = `${authBaseUrl}${actionUrl}`;
+      const body: Record<string, string> = {};
+      for (let _i = 0, _a = Array.prototype.slice.call(inputs); _i < _a.length; _i++) {
+          const input = _a[_i];
+          body[input.name] = input.value;
+      }
+      return cy.request({
+          method: "POST",
+          url: url,
+          followRedirect: false,
+          form: true,
+          body: body
+      });
   });
 });
